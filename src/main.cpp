5b4f48f0bd3508d56725d5fa7f799ddc5dafeeaf
Ali Mahgoub <96558364+ali-mahgoub@users.noreply.github.com>
2024-11-10 20:28:53 -0500

    WIP: Added and modified the read_planes function
    There are some issues, not sure if its due to overloading or a problem with how the data is being parsed, but this wasn't as big an issue in earlier versions.

--- src/main.cpp
+++ src/main.cpp
@@ -9,6 +9,11 @@
 #include "ComputerSystem.h"
 #include "vector.h"
 #include "messages.h"
+#include <fstream>
+#include <string>
+#include <sstream>
+
+void read_planes(Radar& radar);
 
 int main() {
     // Create ComputerSystem
@@ -31,6 +36,7 @@ int main() {
     radar.add_plane("Plane2", Vector(7, 8, 9), Vector(0, 100, 0));
     radar.add_plane("Plane3", Vector(13, 14, 15), Vector(0, 0, 100));
     radar.add_plane("Plane4", Vector(19, 20, 21), Vector(-100, -100, -100));
+    read_planes(radar);
 
     // Simulation runs for 30 seconds
     sleep(30);
@@ -43,6 +49,42 @@ int main() {
     ConnectDetach(computerSystemRadarCoid);
 
     return 0;
+}
+
+//read and create planes from planes.txt file
+//IDs are hardcoded for now, they are in the .txt file.
+void read_planes(Radar& radar) {
+	std::string filePath = "./planes.txt";
+	std::ifstream plane_file;
+	plane_file.open(filePath);
+	Vector Position;
+	Vector Velocity;
+	std::string ID;
+
+
+	if(!plane_file.is_open()){
+		std::cerr << "Error: Could not open file " << filePath << std::endl;
+	}
+
+	std::string line;
+	    while (std::getline(plane_file, line, ';'))
+	    {  // Split each line by semicolons
+	    		std::vector<std::string> row;
+	    		std::stringstream ss(line);
+	    		std::string value;
+
+	    	        // Split each entry in the line by commas
+	    	        while (std::getline(ss, value, ',')) {
+	    	            row.push_back(value);
+	    	        }
+	    	    ID = row[0];
+	    	    Position = {std::stof(row[1]), std::stof(row[2]), std::stof(row[3])};
+	    	    Velocity = {std::stof(row[4]), std::stof(row[5]), std::stof(row[6])};
+	    	    std::cout<< row[0] << std::endl;
+	    	    radar.add_plane(ID, Velocity, Position);
+	    }
+
+	    plane_file.close();
 }
 
 
